109fb416d51f9525e2ebc8a0e092c0b39af8875d
rcerv87 <mr.rubencervantes@gmail.com>
2021-11-29 15:55:18 -0800

    edit

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML, 
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
